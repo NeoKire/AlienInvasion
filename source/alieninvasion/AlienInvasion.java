0b3d2dbfd469eb32b13b8ced223ebb2c5a4d666e
Kieran Regan <NeoKire@hotmail.com>
2014-12-13 20:18:30 +0000

    Added instance

--- source/alieninvasion/AlienInvasion.java
+++ source/alieninvasion/AlienInvasion.java
@@ -1,9 +1,13 @@
 package alieninvasion;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Instance;
 
 @Mod(modid=AlienInvasion.MODID, version=AlienInvasion.VERSION)
 public class AlienInvasion {
 	public static final String MODID = "alieninvasion";
 	public static final String VERSION = "0.1";
+	
+	@Instance(value="alieninvasion")
+	public static AlienInvasion instance;
 }
